6e647c9fb094cd472da8402580137d2a58fd87b9
Kyle Bartush <bartushk@gmail.com>
2016-08-06 21:38:18 -0400

    Added a unit test and the timebomb package for unit test timeouts.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
 pub mod service;
 
-use service::key_logger::linux_key_logger::LinuxKeyLoggerBuilder;
-use service::key_logger::LoggerBuilder;
-
 fn main() {
-    let (logger, channel) = LinuxKeyLoggerBuilder::new();
-    logger.start_logging();
-    for _ in 0..10 {
-        let result = channel.recv().unwrap();
-        println!("{}", result);
-    }
+
 }
--- src/service/key_logger/linux_key_logger/mod.rs
+++ src/service/key_logger/linux_key_logger/mod.rs
@@ -1,18 +1,24 @@
+extern crate timebomb;
+
 use std::sync::mpsc;
 use std::thread;
+
+use self::timebomb::timeout_ms;
+
 use super::{ LoggerBuilder, KeyLogger};
+
 
 
 pub struct LinuxLogger {
     sender: mpsc::Sender<&'static str>,
 }
 
-pub struct LinuxKeyLoggerBuilder {
+pub struct LinuxLoggerBuilder {
 
 }
 
 
-impl LoggerBuilder for LinuxKeyLoggerBuilder {
+impl LoggerBuilder for LinuxLoggerBuilder {
     fn new() -> (Box<KeyLogger>, mpsc::Receiver<&'static str>) {
         let (tx, rx) = mpsc::channel();
         (Box::new(LinuxLogger{sender: tx}), rx)
@@ -24,12 +30,34 @@ impl KeyLogger for LinuxLogger {
     fn start_logging(&self) {
         let send_clone = self.sender.clone();
         thread::spawn( move || {
+            send_clone.send("Logging started.").unwrap();
             for _ in 0..10 {
                 //TODO: Actually log keys and send them up the channel.
-                send_clone.send("asdf").unwrap();
                 thread::sleep_ms(1000);
             }
         });
     }
 }
 
+fn timer_periodic(ms: u32) -> mpsc::Receiver<()> {
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        thread::sleep_ms(ms);
+        tx.send(());
+    });
+    rx
+}
+
+
+
+
+
+#[test]
+fn LinuxKeyLogger_start_logging_LoggingStarted() {
+    timeout_ms(|| {
+        let (logger, rx) =  LinuxLoggerBuilder::new();
+        logger.start_logging();
+        let result = rx.recv().unwrap();
+        assert_eq!(result, "Logging started.");
+    }, 1000);
+}
